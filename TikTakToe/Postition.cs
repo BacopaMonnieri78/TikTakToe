a95c3b7508fed3bd510d71f4518b40e1d804d981
BacopaMonnieri78 <tracrytocurrency@yahoo.com>
2021-07-08 21:18:27 +0200

    Add Class Position

---
+++ TikTakToe/Postition.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TikTakToe
+{
+    public class Postition
+    {
+        public string X { get; set; }
+        public string Y { get; set; }
+    }
+}
